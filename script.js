406f5d492830517d048c5134fdbcb0b8979ed23a
Hatem Soliman <hatemthedev@gmail.com>
2022-02-08 16:42:50 +0200

    Organising files' contents

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//FIRST PAGE
 //VARIABLES
 const signInBtn = document.getElementById("sign-in");
 const signOutBtn = document.getElementById("sign-out");
@@ -56,13 +57,21 @@ signOutBtnTwo.addEventListener("click", () => {
 
 
 
-//Food API & Results Page
+
+
+
+
+//SECOND PAGE
+//VARIABLES
 const searchBtn = document.getElementById('search-btn');
 const mealList = document.getElementById('meal');
 const mealDetailsContent = document.querySelector('.meal-details-content');
 const recipeCloseBtn = document.getElementById('recipe-close-btn');
 
-// event listeners
+
+
+
+//EVENT LISTENERS
 searchBtn.addEventListener('click', getMealList);
 mealList.addEventListener('click', getMealRecipe);
 recipeCloseBtn.addEventListener('click', () => {
@@ -70,7 +79,10 @@ recipeCloseBtn.addEventListener('click', () => {
 });
 
 
-// get meal list that matches with the ingredients
+
+
+//FUNCTIONS
+//Get Meal List that Matches with the Ingredients
 function getMealList(){
     let searchInputTxt = document.getElementById('search-input').value.trim();
     fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${searchInputTxt}`)
@@ -86,7 +98,7 @@ function getMealList(){
                         </div>
                         <div class = "meal-name">
                             <h3>${meal.strMeal}</h3>
-                            <a href = "#" class = "recipe-btn">Get Recipe</a>
+                            <a href = "#" class = "recipe-btn">Add to Cart</a>
                         </div>
                     </div>
                 `;
@@ -99,4 +111,39 @@ function getMealList(){
 
         mealList.innerHTML = html;
     });
+}
+
+
+//Get Recipe of the Meal
+function getMealRecipe(e){
+    e.preventDefault();
+    if(e.target.classList.contains('recipe-btn')){
+        let mealItem = e.target.parentElement.parentElement;
+        fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealItem.dataset.id}`)
+        .then(response => response.json())
+        .then(data => mealRecipeModal(data.meals));
+    }
+}
+
+
+//Create a Modal
+function mealRecipeModal(meal){
+    console.log(meal);
+    meal = meal[0];
+    let html = `
+        <h2 class = "recipe-title">${meal.strMeal}</h2>
+        <p class = "recipe-category">${meal.strCategory}</p>
+        <div class = "recipe-instruct">
+            <h3>Instructions:</h3>
+            <p>${meal.strInstructions}</p>
+        </div>
+        <div class = "recipe-meal-img">
+            <img src = "${meal.strMealThumb}" alt = "">
+        </div>
+        <div class = "recipe-link">
+            <a href = "${meal.strYoutube}" target = "_blank">Watch Video</a>
+        </div>
+    `;
+    mealDetailsContent.innerHTML = html;
+    mealDetailsContent.parentElement.classList.add('showRecipe');
 }
